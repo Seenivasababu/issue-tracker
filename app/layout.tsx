2625dbdcc0eec3485825b1037a71d222313ca76e
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-10-24 23:27:41 +0530

    Refactor moving inline function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import '@radix-ui/themes/styles.css';
-import './theme.config.css'
+import './theme.config.css';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './Navbar';
 import { Theme, ThemePanel } from '@radix-ui/themes';
 
-const inter = Inter({ 
-  subsets: ['latin'] ,
-  variable : '--font-inter'
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -27,7 +27,6 @@ export default function RootLayout({
         <Theme appearance="light" accentColor="iris">
           <Navbar />
           <main className="p-5">{children}</main>
-          
         </Theme>
       </body>
     </html>
