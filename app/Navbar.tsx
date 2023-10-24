8f70136e502c138e2602a91a65be870af6362796
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-10-24 21:33:10 +0530

    Add a markdown editor

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -23,9 +23,9 @@ const Navbar = () => {
             key={link.href}
             href={link.href}
             className={classnames({
-              'text-zinc-900' : link.href === currentPath,
-              'text-zinc-500' : link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors' : true 
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
             })}
           >
             {link.label}
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,19 @@
-'use client'
-import React from 'react'
-import {TextField,TextArea,Button} from '@radix-ui/themes'
+'use client';
+import React from 'react';
+import { TextField,  Button } from '@radix-ui/themes';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
-    <div className='max-w-xl space-y-4'>
+    <div className="max-w-xl space-y-4">
       <TextField.Root>
-         <TextField.Input placeholder='Title'/>
+        <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Description"/>
+      <SimpleMDE placeholder="Description" />
       <Button> Submit New Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
