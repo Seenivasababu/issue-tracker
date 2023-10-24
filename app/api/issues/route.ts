648247c705e15a11c379e0735a6403ca100e812b
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-10-24 23:00:57 +0530

    Implement client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,22 +1,16 @@
-import { NextRequest,NextResponse } from "next/server";
-import {z} from 'zod'
-import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/prisma/client';
+import { createIssueScheme } from '../../validationSchema';
 
-const createIssueScheme = z.object({
-   title : z.string().min(1,'Title is required').max(255),
-   description : z.string().min(1,'Description is required')
-})
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const validation = createIssueScheme.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
 
-export async function POST(req : NextRequest){
-   const body = await req.json()
-   const validation = createIssueScheme.safeParse(body)
-   if(!validation.success){
-      return NextResponse.json(validation.error.format(), {status:400})
-   }
-   const newIssue = await prisma.issue.create({
-      data : {title:body.title,description:body.description}
-   })
-
-   return NextResponse.json(newIssue, {status:201})
-
+  return NextResponse.json(newIssue, { status: 201 });
 }
