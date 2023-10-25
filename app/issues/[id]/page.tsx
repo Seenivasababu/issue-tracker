cd58da0012644706484f5aa1cf3f15daceeb4597
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-10-25 21:04:28 +0530

    Style the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,28 +1,33 @@
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
-import React from 'react'
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import prisma from '@/prisma/client';
+import { Heading, Card, Flex, Text } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+import React from 'react';
 
 interface Props {
-   params : {id : string}
+  params: { id: string };
 }
-const IssueDetailPage = async ({params}:Props) => {
-  
-   const issue = await prisma.issue.findUnique({
-      where : {id: parseInt(params.id)}
-   })
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-   if(!issue){
-      notFound()
-   }
+  if (!issue) {
+    notFound();
+  }
 
-   return (
-      <div>
-        <p>{issue.title}</p>
+  return (
+    <div>
+      <Heading> {issue.title} </Heading>
+      <Flex className="space-x-3 my-2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
         <p>{issue.description}</p>
-        <p>{issue.status}</p>
-        <p>{issue.createdAt.toDateString()}</p>
-      </div>
-    )
-}
+      </Card>
+    </div>
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
