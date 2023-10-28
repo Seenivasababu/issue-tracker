e1f1703d6274d16257065b725c0188963eca205a
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-10-28 11:09:59 +0530

    Created Issue Summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Image from 'next/image'
 import LatestIssues from './LatestIssue'
+import IssueSummary from './IssueSummary'
+import prisma from '@/prisma/client';
+import { Flex } from '@radix-ui/themes'
 
-export default function Home() {
+export default async function Home() {
+
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  });
   return (
     <main className="">
-      Hello world
+      <Flex direction='column' gap='3'>
+
+      
+      <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
       <LatestIssues/>
+      </Flex>
     </main>
   )
 }
