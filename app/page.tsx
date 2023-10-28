0f81a9deb368856da24d093a951cd154adc12723
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-10-28 10:48:42 +0530

    Created the Latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import LatestIssues from './LatestIssue'
 
 export default function Home() {
   return (
     <main className="">
       Hello world
+      <LatestIssues/>
     </main>
   )
 }
